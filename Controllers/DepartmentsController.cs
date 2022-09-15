17814f3e636a32acdb3d17b0dff2fb8bb9b197cf
vinodh1988 <k.c.vinodh@hotmail.com>
2022-09-15 16:23:06 +0530

    relationship-post

--- Controllers/DepartmentsController.cs
+++ Controllers/DepartmentsController.cs
@@ -1,7 +1,9 @@
 ﻿using CrudAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -24,7 +26,26 @@ namespace CrudAPI.Controllers
         public async Task<ActionResult<IEnumerable<Department>>> Get()
         {
 
-            return await _context.departments.Include(d=>d.Employees).ToListAsync();
+            return await _context.departments.Include(d => d.Employees).ToListAsync();
         }
+
+        [HttpPost]
+        public async Task<ActionResult<ActionResult<Department>>> Post(Department dept){
+            try
+            {
+                Console.WriteLine(dept.Employees.Count);
+                _context.departments.Add(dept);
+                foreach (Employee x in dept.Employees)
+                {
+                    x.Dno = dept.Dno;
+                }
+                
+                await _context.SaveChangesAsync();
+                return Ok(dept);
+            }
+            catch (Exception e) { 
+                return StatusCode(500, e.Message);
+            }
+         }
     }
 }
